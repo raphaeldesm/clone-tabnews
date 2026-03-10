896713faaf41c053448c8d93cc57bdf06ed4e7ed
Raphael de Souza Marinho <108803673+raphaeldesm@users.noreply.github.com>
2026-03-10 20:59:14 +0000

    fix `eslint` linting

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -9,14 +9,18 @@ async function waitForAllServices() {
       maxTimeout: 1000,
     });
 
-    async function fetchStatusPage(bail, tryNumber) {
-      console.log(tryNumber);
+    async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
-      const responseBody = await response.json();
+
+      if (response.status !== 200) {
+        throw Error();
+      }
     }
   }
 }
 
-export default {
+const orchestrator = {
   waitForAllServices,
 };
+
+export default orchestrator;
