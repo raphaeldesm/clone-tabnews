d006a9a08f377236eb2a1482101c7d234454f9e7
Raphael de Souza Marinho <108803673+raphaeldesm@users.noreply.github.com>
2026-03-10 20:09:58 +0000

    fix `prettier` style linting

--- infra/scripts/wait-for-postgres.js
+++ infra/scripts/wait-for-postgres.js
@@ -10,7 +10,7 @@ function checkPostgres() {
       return;
     }
 
-    console.log("\n🟢 Postgres está pronto e aceitando conexões!")
+    console.log("\n🟢 Postgres está pronto e aceitando conexões!");
   }
 }
 
