56bbfa1c3f156325f31c367859653c2a6f593c4a
Raphael de Souza Marinho <108803673+raphaeldesm@users.noreply.github.com>
2026-04-27 16:16:19 +0000

    feat: add migration to create `users` table

--- infra/migrations/1777299880666_create-users.js
+++ infra/migrations/1777299880666_create-users.js
@@ -20,22 +20,24 @@ exports.up = (pgm) => {
       unique: true,
     },
 
-    // Why 72 in length? https://security.stackexchange.com/a/39851
+    // Why 60 in length? https://www.npmjs.com/package/bcrypt#hash-info
     password: {
-      type: "varchar(72)",
+      type: "varchar(60)",
       notNull: true,
     },
 
-    // Why timestamp with timezone: https://justatheory.com/2012/04/postgres-use-timestamptz/
+    // Why timestamp with timezone? https://justatheory.com/2012/04/postgres-use-timestamptz/
     created_at: {
       type: "timestamptz",
-      default: pgm.func("now()")
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
     },
 
     updated_at: {
       type: "timestamptz",
-      default: pgm.func("now()"),
-    }
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
+    },
   });
 };
 
